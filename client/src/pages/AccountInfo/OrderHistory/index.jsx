c6a06c92420c4af50cb83a0afec8f2cef3edfced
Arina <petrinhtrinh182@gmail.com>
2021-10-23 14:35:08 +0700

    done account info

---
+++ client/src/pages/AccountInfo/OrderHistory/index.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { orderDisplayState } from '../../../recoil/orderDisplayState';
+import { userState } from '../../../recoil/userState';
+import OrderDetail from '../../../shared/OrderDetail';
+import { useQuery } from 'react-query';
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+import ReactPaginate from "react-paginate";
+import orderApi from '../../../apis/orderApi';
+
+function OrderHistory() {
+  const [orderDisplay, setOrderDisplay] = useRecoilState(orderDisplayState);
+
+  const [currentOrder, setCurrentOrder] = useState({});
+  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(0);
+
+  const user = useRecoilValue(userState);
+
+  const { data: orders, isLoading } = useQuery(['userOrders', page], async () => {
+    const params = {
+      page: page + 1,
+      limit: 8
+    }
+    const response = await orderApi.getByUserId(user._id, params);
+    setTotalPages(response.totalPages);
+    return response.orders;
+  });
+
+  const handlePageChange = ({ selected }) => {
+    setPage(selected);
+  };
+
+  const handleViewDetailClick = (order) => {
+    setCurrentOrder(order);
+    setOrderDisplay(true);
+  }
+
+  return (
+    <React.Fragment>
+      <div className="info-title">Lịch sử mua hàng</div>
+      <div className="divider"></div>
+
+      <table className="order-history" width="100%">
+        <thead>
+          <tr>
+            <th>Mã đơn hàng</th>
+            <th>Ngày</th>
+            <th>Tổng đơn</th>
+            <th>Trạng thái</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {orders?.map(order => (
+            <tr key={order._id}>
+              <td>{order.idOrder}</td>
+              <td>{new Date(order.date).toLocaleString("en-GB").slice(0, 10)}</td>
+              <td>{order.total.toLocaleString()}đ</td>
+              <td>{getStatus(order.status)}</td>
+              <td><span className="view-detail-btn" onClick={() => handleViewDetailClick(order)}>Xem chi tiết</span></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {orders?.length > 0 && totalPages > 1 &&
+        <ReactPaginate
+          previousLabel={<GrFormPrevious className="prev-icon" />}
+          nextLabel={<GrFormNext className="next-icon" />}
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination"}
+          pageClassName={"paginated-btn"}
+          breakClassName={"paginated-btn"}
+          previousClassName={"prev-btn"}
+          nextClassName={"next-btn"}
+          disabledClassName={"disabled-btn"}
+          activeClassName={"active-btn"}
+          forcePage={page}
+        />}
+
+      {orderDisplay && <OrderDetail order={currentOrder} />}
+    </React.Fragment>
+  );
+}
+
+const getStatus = (status) => {
+  switch (status) {
+    case 0: return 'Chờ xác nhận';
+    case 1: return 'Đã xác nhận';
+    case 2: return 'Đã thanh toán';
+    case 3: return 'Giao thành công';
+    case 4: return 'Đã hủy';
+    default: return null;
+  }
+}
+
+export default OrderHistory;
---
+++ client/src/shared/OrderDetail/index.jsx
@@ -0,0 +1,77 @@
+import './OrderDetail.scss';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { orderDisplayState } from '../../recoil/orderDisplayState';
+import { IoClose } from "react-icons/io5";
+
+function OrderDetail({ order }) {
+  const setOrderDisplay = useSetRecoilState(orderDisplayState);
+
+  return (
+    <React.Fragment>
+      <div id="overlay" onClick={() => setOrderDisplay(false)}></div>
+      <div className="order-detail">
+        <div className="order-cart-list" width="100%">
+          <div className="heading-id">Đơn hàng <span>#{order.idOrder}</span></div>
+          <div className="heading-title">
+            <div className="product-color">Ảnh</div>
+            <div className="product-description">Sản phẩm</div>
+            <div className="product-quantity">Số lượng</div>
+            <div className="product-unit-price">Đơn giá</div>
+            <div className="product-discount">Khuyến mãi</div>
+            <div className="product-total-price">Thành tiền</div>
+          </div>
+
+          <div className="order-cart-detail">
+            {order.products.map(product => (
+              <div className="item-row" key={product._id}>
+                <div className="product-color-container">
+                  <div className="product-color" style={{ backgroundImage: `url(${product.color})` }}></div>
+                </div>
+                <div className="product-description">{`${product.name} - ${product.size}`}</div>
+                <div className="product-quantity">{product.soldQuantity}</div>
+                <div className="product-unit-price">{product.price.toLocaleString()}đ</div>
+                <div className="product-discount">{product.discount.toLocaleString()}đ</div>
+                <div className="product-total-price">{((product.price - product.discount) * product.soldQuantity).toLocaleString()}đ</div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="extra-info">
+          <ul className="buyer-info col-60">
+            <li className="name">Tên người nhận: <span>{order.receiverInfo.name}</span></li>
+            <li className="phone">Điện thoại: <span>{order.receiverInfo.phone}</span></li>
+            <li className="email">Email: <span>{order.receiverInfo.email}</span></li>
+            <li className="note">Ghi chú: <span>{order.receiverInfo.note ? order.receiverInfo.note : 'Không có'}</span></li>
+            <li className="address">Địa chỉ giao hàng: <span>{order.receiverInfo.address}</span></li>
+          </ul>
+          <ul className="payment-info col-40">
+            <li className="total-price">Tổng đơn: <span>{order.total.toLocaleString()}đ</span></li>
+            <li className="shipping-fee">Phí vận chuyển: <span>{order.shipFee.toLocaleString()}đ</span></li>
+            <li className="payment-method">Trạng thái: <span>{getStatus(order.status)}</span></li>
+            <li className="date">Ngày đặt: {new Date(order.date).toLocaleString("en-GB").slice(0, 10)}</li>
+            <li className="payment-method">{order.paymentMethod}</li>
+          </ul>
+        </div>
+
+        <div className="close-btn" onClick={() => setOrderDisplay(false)}>
+          <IoClose className="close-icon" />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+const getStatus = (status) => {
+  switch (status) {
+    case 0: return 'Chờ xác nhận';
+    case 1: return 'Đã xác nhận';
+    case 2: return 'Đã thanh toán';
+    case 3: return 'Giao thành công';
+    case 4: return 'Đã hủy';
+    default: return null;
+  }
+}
+
+export default OrderDetail;
